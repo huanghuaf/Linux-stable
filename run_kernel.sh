e1d515164aaa09b06619cc9271e0eb04976a181d
huanghuaf <1570691736@qq.com>
2018-10-09 23:34:31 +0800

    kernel:pass bootargs by dts instead of qeum-arm append args.
    Signed-off-by: huanghuaf <1570691736@qq.com>

--- run_kernel.sh
+++ run_kernel.sh
@@ -1,1 +1,1 @@
-qemu-system-arm -M vexpress-a9 -smp 4 -m 1024 -kernel arch/arm/boot/zImage -append "rdinit=/linuxrc console=ttyAMA0 loglevel=8" -dtb arch/arm/boot/dts/vexpress-v2p-ca9.dtb -nographic
+qemu-system-arm -M vexpress-a9 -smp 4 -m 1024 -kernel arch/arm/boot/zImage -dtb arch/arm/boot/dts/vexpress-v2p-ca9.dtb -nographic
