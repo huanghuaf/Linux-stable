774fab29134a8fff22dac8094cea4174cc48dd09
huanghuaf <1570691736@qq.com>
2019-02-04 12:24:10 +0800

    build.sh:fix some error of build kernel.
    Signed-off-by: huanghuaf <1570691736@qq.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabi-
-#make vexpress_defconfig
+if [ ! -f .config ];then
+	make vexpress_defconfig
+fi
+
 make bzImage -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 make dtbs
 
