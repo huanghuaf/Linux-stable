4ef459e63bf62873950a1ce92b66b373f1b000d7
huanghuaf <1570691736@qq.com>
2022-03-25 22:51:20 +0800

    add net device for qemu arm virt-machine.
    Signed-off-by: huanghuaf <1570691736@qq.com>

--- run_kernel.sh
+++ run_kernel.sh
@@ -1,1 +1,1 @@
-qemu-system-arm -M vexpress-a9 -smp 4 -m 1024 -kernel arch/arm/boot/zImage -dtb arch/arm/boot/dts/vexpress-v2p-ca9.dtb -nographic
+sudo qemu-system-arm -M vexpress-a9 -smp 4 -m 1024 -kernel arch/arm/boot/zImage -dtb arch/arm/boot/dts/vexpress-v2p-ca9.dtb -nographic -net nic -net tap,ifname=tap1,script=/etc/qemu-ifup
