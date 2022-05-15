834853cc34158e04c4382c168101bfe899f15226
huanghuaf <1570691736@qq.com>
2022-05-15 16:03:04 +0800

    change build_ramfs.sh for rich function.
    Signed-off-by: huanghuaf <1570691736@qq.com>

--- build_ramfs.sh
+++ build_ramfs.sh
@@ -1,19 +1,41 @@
-  #!/bin/sh
+#!/bin/bash
 
-  # Licensed under GPL version 2
+set -e
 
-  if [ $# -ne 2 ]
-  then
-    echo "usage: mkinitramfs directory imagename.cpio.gz"
-    exit 1
-  fi
+generate_rootfs()
+{
+	if [ -d skel ] ; then
+		(cd skel; find . | fakeroot cpio -o -Hnewc > ../"$1")
+	else
+		echo "skel not exist"
+		exit 1
+	fi
+}
 
-  if [ -d "$1" ]
-  then
-    echo "creating $2 from $1"
-    (cd "$1"; find . | cpio -o -H newc | gzip) > "$2"
-  else
-    echo "First argument must be a directory"
-    exit 1
-  fi
+extract_rootfs()
+{
+	if [ -f "$1" ] ; then
+		rm -rf skel && mkdir skel
+		(cd skel; fakeroot cpio -i -Hnewc <../$1)
+	else
+		echo "$1 not exist"
+		exit 1
+	fi
+}
 
+if [ $# -ne 2 ]; then
+	echo -e "please input correct parameters"
+	echo -e "\t[build.sh e rootf.cpio] to extract the rootfs template to skel folder"
+	echo -e "\tthen make some changes in the skel folder"
+	echo -e "\t[build.sh c rootf.cpio] to create the rootfs from the skel folder"
+	exit 1
+fi
+
+if [ "$1" = "e" ] ; then
+	extract_rootfs $2
+elif [ "$1" = "c" ] ; then
+	generate_rootfs $2
+else
+	echo "Wrong arguments"
+	exit 1
+fi
