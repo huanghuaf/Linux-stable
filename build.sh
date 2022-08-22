3c9ee9d9cadf270378d5b9f1c476b31a7fd4b759
huafeng.huang <huafeng.huang@semidrive.com>
2022-08-22 05:21:17 -0700

    Support build arm64
    Signed-off-by: huafeng.huang <huafeng.huang@semidrive.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
 
-export ARCH=arm
-export CROSS_COMPILE=arm-linux-gnueabi-
-if [ ! -f .config ];then
-	make vexpress_defconfig
+NR_CPUS=`cat /proc/cpuinfo| grep "processor"| wc -l`
+
+if [ ! -f .config ]; then
+	if [ "x$1" = "xarm" ]; then
+		make ARCH=$1 vexpress_defconfig
+	elif [ "x$1" = "xarm64" ]; then
+		make ARCH=arm64 defconfig
+	else
+		echo "Wrong arguments"
+		exit 1
+	fi
+else
+	if [ "x$1" = "xarm" ]; then
+		if [ "x" = "x$(cat .config | grep CONFIG_ARM=y)" ]; then
+			make distclean;
+			make ARCH=$1 vexpress_defconfig
+		else
+			echo "Not support ARCH $1"
+		fi
+	elif [ "x$1" = "xarm64" ]; then
+		if [ "x" = "x$(cat .config | grep CONFIG_ARM64=y)" ]; then
+			make distclean;
+			make ARCH=$1 defconfig
+		else
+			echo "Not support ARCH $1"
+		fi
+	else
+		echo "Not support ARCH $1"
+	fi
 fi
 
-make bzImage -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
-make dtbs
-
+if [ "x$1" = "xarm" ]; then
+	make -j$NR_CPUS ARCH=$1 CROSS_COMPILE=arm-linux-gnueabi- bzImage dtbs
+elif [ "x$1" = "xarm64" ]; then
+	make -j$NR_CPUS ARCH=$1 CROSS_COMPILE=aarch64-linux-gnu- Image dtbs
+else
+	echo "Wrong arguments"
+	exit 1
+fi
